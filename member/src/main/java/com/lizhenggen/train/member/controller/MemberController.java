e10e013cf34cac88585c805d8bd6335363cf1842
cangjie <762741539@qq.com>
2023-10-08 17:39:52 +0800

    4.15 实现一个简单的注册接口

--- member/src/main/java/com/lizhenggen/train/member/controller/MemberController.java
+++ member/src/main/java/com/lizhenggen/train/member/controller/MemberController.java
@@ -4,6 +4,7 @@ package com.lizhenggen.train.member.controller;
 import com.lizhenggen.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,10 @@ public class MemberController {
     @GetMapping("/count")
     public Integer hello(){
         return memberService.count();
+    }
+
+    @PostMapping("/register")
+    public long register(String mobile){
+        return memberService.register(mobile);
     }
 }
