d7c3e75fcbdaafc73831cf0e3b40dfb7022677a3
cangjie <762741539@qq.com>
2023-10-09 11:24:44 +0800

    4.20 使用雪花算法生成数据库id

--- member/src/main/java/com/lizhenggen/train/member/service/MemberService.java
+++ member/src/main/java/com/lizhenggen/train/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.lizhenggen.train.member.service;
 import cn.hutool.core.collection.CollUtil;
 import com.lizhenggen.train.common.exception.BusinessException;
 import com.lizhenggen.train.common.exception.BusinessExceptionEnum;
+import com.lizhenggen.train.common.util.SnowUtil;
 import com.lizhenggen.train.member.domain.Member;
 import com.lizhenggen.train.member.domain.MemberExample;
 import com.lizhenggen.train.member.mapper.MemberMapper;
@@ -35,7 +36,9 @@ public class MemberService {
 
         Member member = new Member();
 //        member.setId(1L);
-        member.setId(System.currentTimeMillis());
+//        member.setId(System.currentTimeMillis());
+
+        member.setId(SnowUtil.getSnowflakeNextId());
         member.setMobile(mobile);
         memberMapper.insert(member);
         return  member.getId();
