1156d7ffcae082b2d0541f0ac1aabca8de61723c
cangjie <762741539@qq.com>
2023-10-09 10:27:00 +0800

    4.19 集成校验框架validation

--- common/src/main/java/com/lizhenggen/train/common/controller/ControllerExceptionHandler.java
+++ common/src/main/java/com/lizhenggen/train/common/controller/ControllerExceptionHandler.java
@@ -4,9 +4,12 @@ import com.lizhenggen.train.common.exception.BusinessException;
 import com.lizhenggen.train.common.resp.CommonResp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 /**
  * 统一异常处理、数据预处理等
@@ -51,14 +54,14 @@ public class ControllerExceptionHandler {
      * @param e
      * @return
      */
-//    @ExceptionHandler(value = BindException.class)
-//    @ResponseBody
-//    public CommonResp exceptionHandler(BindException e) {
-//        CommonResp commonResp = new CommonResp();
-//        LOG.error("校验异常：{}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
-//        commonResp.setSuccess(false);
-//        commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
-//        return commonResp;
-//    }
+    @ExceptionHandler(value = BindException.class)
+    @ResponseBody
+    public CommonResp exceptionHandler(BindException e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("校验异常：{}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        return commonResp;
+    }
 
 }
--- member/src/main/java/com/lizhenggen/train/member/controller/MemberController.java
+++ member/src/main/java/com/lizhenggen/train/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.lizhenggen.train.common.resp.CommonResp;
 import com.lizhenggen.train.member.req.MemberRegisterReq;
 import com.lizhenggen.train.member.service.MemberService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +27,7 @@ public class MemberController {
     }
 
     @PostMapping("/register")
-    public CommonResp<Long> register(MemberRegisterReq req){
+    public CommonResp<Long> register(@Valid MemberRegisterReq req){
         long register = memberService.register(req);
 //        CommonResp<Long> commonResp = new CommonResp<>();
 //        commonResp.setContent(register);
